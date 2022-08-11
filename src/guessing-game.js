e913904c8f9e8d280bc325ceb3b00bffbde2ac05
Alex <a.tsiklauri@gmail.com>
2022-08-11 14:31:36 +0100

    feat: upload solved task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
+
 class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+      this.minimum = min;
+      this.maximum = max;
     }
 
     guess() {
-
+      this.result = Math.round((this.maximum + this.minimum)/ 2);
+      return this.result
     }
 
     lower() {
-
+      this.setRange(this.minimum, this.result)
     }
 
     greater() {
-
+      this.setRange(this.result, this.maximum)
     }
 }
 
